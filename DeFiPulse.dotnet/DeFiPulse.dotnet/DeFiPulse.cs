24eebe0e9f0b77b51e68707173b8c09649bc31c8
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-29 21:02:47 -0300

    Create DeFiPulse class

---
+++ DeFiPulse.dotnet/DeFiPulse.dotnet/DeFiPulse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using DeFiPulse.Entities;
+
+namespace DeFiPulse
+{
+    public class DeFiPulse : IDeFiPulse
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public DeFiPulse(HttpClient httpClient, string apiKey)
+        {
+            _httpClient = httpClient;
+            _apiKey = apiKey;
+        }
+
+        public async Task<GasPrice> GetGasPriceAsync()
+        {
+            var result =  await _httpClient.GetFromJsonAsync<GasPrice>($"egs/api/ethgasAPI.json?api-key={_apiKey}");
+            return result;
+        }
+    }
+}
