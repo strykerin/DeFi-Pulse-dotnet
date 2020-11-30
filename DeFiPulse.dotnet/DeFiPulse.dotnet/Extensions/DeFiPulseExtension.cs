8fed043a5a898da3bdec28fb209bf609183e67d5
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-29 21:02:13 -0300

    Create DeFiPulse extension method

---
+++ DeFiPulse.dotnet/DeFiPulse.dotnet/Extensions/DeFiPulseExtension.cs
@@ -0,0 +1,22 @@
+﻿using DeFiPulse.Parameters;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+
+namespace DeFiPulse.Extensions
+{
+    public static class DeFiPulseExtension
+    {
+        public static void AddDeFiPulse(this IServiceCollection services, string apiKey)
+        {
+            services.AddHttpClient("DeFiPulse", c => c.BaseAddress = new Uri(DeFiPulseParameters.BaseAddress));
+            services.AddScoped<IDeFiPulse>(ctx =>
+            {
+                var clientFactory = ctx.GetRequiredService<IHttpClientFactory>();
+                var httpClient = clientFactory.CreateClient("DeFiPulse");
+
+                return new DeFiPulse(httpClient, apiKey);
+            });
+        }
+    }
+}
