2f805dd70c58e5bdc83bcbee3412699d7a186933
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-29 21:30:38 -0300

    Add XML comments to GetGasPriceAsync

--- DeFiPulse.dotnet/DeFiPulse.dotnet/IDeFiPulse.cs
+++ DeFiPulse.dotnet/DeFiPulse.dotnet/IDeFiPulse.cs
@@ -5,6 +5,10 @@ namespace DeFiPulse
 {
     public interface IDeFiPulse
     {
+        /// <summary>
+        /// Returns the current recommended fast, standard and safe low gas prices on the Ethereum network, along with the current block and wait times for each "speed".
+        /// </summary>
+        /// <returns></returns>
         Task<GasPrice> GetGasPriceAsync();
     }
 }
