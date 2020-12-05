b60e95de3dbc9ae8c1522fe6c3abab3eb20dbb32
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-05 13:09:55 -0300

    Add classes for DeFi Projects overview

--- DeFiPulse.dotnet/DeFiPulse.dotnet/Entities/MarketOverview.cs
+++ DeFiPulse.dotnet/DeFiPulse.dotnet/Entities/MarketOverview.cs
@@ -15,7 +15,44 @@ namespace DeFiPulse.Entities
         [JsonPropertyName("dominance_value")]
         public decimal DominanceValue { get; set; }
 
-        [JsonPropertyName("")]
-        public decimal DominanceValue { get; set; }
+        public Value Value { get; set; }
+
+        [JsonPropertyName("dominance_pct")]
+        public decimal DominancePercentage { get; set; }
+    }
+
+    public class Value
+    {
+        public Total Total { get; set; }
+        public Balance Balance { get; set; }
+        [JsonPropertyName("tvl")]
+        public Total TVL { get; set; }
+    }
+
+    public class Balance
+    {
+        [JsonPropertyName("ERC20")]
+        public Total ERC20 { get; set; }
+    }
+
+    public class Total
+    {
+        [JsonPropertyName("USD")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Currency USD { get; set; }
+        [JsonPropertyName("ETH")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Currency ETH { get; set; }
+        [JsonPropertyName("BTC")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Currency BTC { get; set; }
+        [JsonPropertyName("DAI")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Currency DAI { get; set; }
+    }
+
+    public class Currency
+    {
+        public decimal Value { get; set; }
     }
 }
