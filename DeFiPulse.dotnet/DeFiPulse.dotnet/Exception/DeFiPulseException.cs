4ea10f17c1f6b8b528d35f805839e2630e6506e6
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-12 17:07:17 -0300

    Create DeFiPulseException

---
+++ DeFiPulse.dotnet/DeFiPulse.dotnet/Exception/DeFiPulseException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DeFiPulse.Exceptions
+{
+    public class DeFiPulseException : Exception
+    {
+        public DeFiPulseException()
+        {
+        }
+
+        public DeFiPulseException(string message) : base(message)
+        {
+        }
+
+        public DeFiPulseException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
