d232c1a1297d5ba163be0db9b2a2da1a3848ad20
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-12 15:22:15 -0300

    Create HistoricalTLV class

---
+++ DeFiPulse.dotnet/DeFiPulse.dotnet/Entities/HistoricalTLV.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DeFiPulse.Entities
+{
+    public class HistoricalTLV
+    {
+        public long Timestamp { get; set; }
+
+        [JsonPropertyName("tvlUSD")]
+        public decimal TVLUSD { get; set; }
+
+        [JsonPropertyName("tvlETH")]
+        public decimal TVLETH { get; set; }
+
+        [JsonPropertyName("ETH")]
+        public decimal ETH { get; set; }
+
+        [JsonPropertyName("DAI")]
+        public decimal DAI { get; set; }
+    }
+}
