5dbab0d64450adc7e1d08ae2db03d3cc35da2781
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-12 15:22:43 -0300

    Create GetHistory method in DeFiPulse class

--- DeFiPulse.dotnet/DeFiPulse.dotnet/DeFiPulse.cs
+++ DeFiPulse.dotnet/DeFiPulse.dotnet/DeFiPulse.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using System.Net.Http.Json;
 using DeFiPulse.Entities;
+using System.Web;
+using System.Collections.Specialized;
+using System.Collections.Generic;
 
 namespace DeFiPulse
 {
@@ -27,9 +29,18 @@ namespace DeFiPulse
             return await _httpClient.GetFromJsonAsync<DeFiProjectsMarketOverview>($"defipulse/api/MarketData?api-key={_apiKey}");
         }
 
-        public async Task GetHistory()
+        public async Task<List<HistoricalTLV>> GetHistory(string project = null, string period = null, string format = null)
         {
-            return await _httpClient.GetFromJsonAsync<>();
+            NameValueCollection query = HttpUtility.ParseQueryString(string.Empty);
+            if (!(project is null))
+                query[$"{nameof(project)}"] = project;
+            if (!(period is null))
+                query[$"{nameof(period)}"] = period;
+            if (!(format is null))
+                query[$"{nameof(format)}"] = format;
+
+            string queryString = query.ToString();
+            return await _httpClient.GetFromJsonAsync<List<HistoricalTLV>>($"defipulse/api/GetHistory?api-key={_apiKey}" + queryString);
         }
     }
 }
